06fe5374cc648434e43f70dd63dfad750822450c
aliceheiman <aheiman@stanford.edu>
2026-01-28 20:46:39 -0800

    Update __init__.py

--- gee_polygons/__init__.py
+++ gee_polygons/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # Import modules that patch the Site class
 from gee_polygons import extract  # adds extract_categorical, extract_continuous, show_layer
